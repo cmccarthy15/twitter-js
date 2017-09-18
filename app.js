e5077480437ee9ffc20427c980bfde46f869753e
Anule Ndukwu <anule.ndukwu@gmail.com>
2017-09-18 18:36:07 -0400

    started adding middleware

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(function(response, request, next){
+  // console.log(request.domain); //verb
+  // console.log(); //route
+  next();
+});
+
 app.get('/', function(request, response){
   response.send('Jambo');
+});
+
+app.get('/news', function(request, response){
+  response.send("You've got news!");
 });
 
 app.listen(3000, function(){
