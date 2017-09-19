8ca12ae882373f6a0178f9b2166428653fb08413
Caryn McCarthy <cmccarthy15@gmail.com>
2017-09-19 10:27:19 -0400

    finished section 3

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
-const chalk = require('chalk');
 
-app.use('/', function(request, response, next){
-  console.log(chalk.pink(request.method), request.url);
-  next();
-});
+const chalk = require('chalk');
+const volleyball = require('volleyball');
+const morgan = require('morgan');
+
+// play with morgan configuration later
+app.use(morgan('tiny'));
+
+// WITHOUT MORGAN OR VOLLEYBALL
+// app.use(function(req, res, next){
+//   console.log(req.method, req.url, res.statusCode);
+//   next();
+// });
+
+// LOOKING FOR A SPECIAL TYPE OF URI
+// app.use('/special', function(req, res, next){
+//   console.log("you're in a special area");
+//   console.log(req.method, req.url, res.statusCode);
+//   next();
+// });
 
 app.get('/', function(request, response){
   response.send('Jambo');
