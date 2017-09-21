477f54a2f69763966fb70a5b792eb6245f86d8d6
Caryn McCarthy <cmccarthy15@gmail.com>
2017-09-21 11:11:04 -0400

    updated for twitter-sql

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*  MY SOLUTION CODE  -- MY SOLUTION CODE -- MY SOLUTION CODE
+
 'use strict';
 
 const express = require('express');
@@ -48,5 +50,54 @@ nunjucks.configure('views', { noCache: true });
 //   next();
 // });
 
+*/
 
 
+
+/* REVIEW SOLUTION --- REVIEW SOLUTION --- REVIEW SOLUTION */
+
+'use strict';
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var makesRouter = require('./routes');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var bodyParser = require('body-parser');
+var socketio = require('socket.io');
+
+// templating boilerplate setup
+app.engine('html', nunjucks.render); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
+nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
+
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+
+
+// start the server
+var server = app.listen(1337, function(){
+  console.log('listening on port 1337');
+});
+var io = socketio.listen(server);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+// modular routing that uses io inside it
+app.use('/', makesRouter(io));
+
+// // manually-written static file middleware
+// app.use(function(req, res, next){
+//   var mimeType = mime.lookup(req.path);
+//   fs.readFile('./public' + req.path, function(err, fileBuffer){
+//     if (err) return next();
+//     res.header('Content-Type', mimeType);
+//     res.send(fileBuffer);
+//   });
+// });
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+/* MY SOLUTION --- MY SOLUTION --- MY SOLUTION
+
 module.exports = function (io){
 
   const express = require('express');
@@ -14,6 +16,14 @@ module.exports = function (io){
  // .gets for each file path and res.sendFile the file
 
   const tweetBank = require('../tweetBank');
+
+  // write a new function (probably using io.on)
+  // that when a newTweet goes out, re-renders
+  // io.sockets.on('newTweet', router.get('/', function(req, res){
+  //   let tweets = tweetBank.list();
+  //   res.render( 'index', { tweets: tweets, showForm: true } );
+  //   console.log(tweets);
+  // }))
 
   router.get('/', function (req, res) {
     let tweets = tweetBank.list();
@@ -58,3 +68,64 @@ module.exports = function (io){
 
   return router;
 };
+
+*/
+
+
+/* REVIEW SOLUTION --- REVIEW SOLUTION --- REVIEW SOLUTION  */
+
+'use strict';
+var express = require('express');
+var router = express.Router();
+var tweetBank = require('../tweetBank');
+
+module.exports = function makeRouterWithSockets (io) {
+
+  // a reusable function
+  function respondWithAllTweets (req, res, next){
+    var allTheTweets = tweetBank.list();
+    res.render('index', {
+      title: 'Twitter.js',
+      tweets: allTheTweets,
+      showForm: true
+    });
+  }
+
+  // here we basically treet the root view and tweets view as identical
+  router.get('/', respondWithAllTweets);
+  router.get('/tweets', respondWithAllTweets);
+
+  // single-user page
+  router.get('/users/:username', function(req, res, next){
+    var tweetsForName = tweetBank.find({ name: req.params.username });
+    res.render('index', {
+      title: 'Twitter.js',
+      tweets: tweetsForName,
+      showForm: true,
+      username: req.params.username
+    });
+  });
+
+  // single-tweet page
+  router.get('/tweets/:id', function(req, res, next){
+    var tweetsWithThatId = tweetBank.find({ id: Number(req.params.id) });
+    res.render('index', {
+      title: 'Twitter.js',
+      tweets: tweetsWithThatId // an array of only one element ;-)
+    });
+  });
+
+  // create a new tweet
+  router.post('/tweets', function(req, res, next){
+    var newTweet = tweetBank.add(req.body.name, req.body.text);
+    io.sockets.emit('new_tweet', newTweet);
+    res.redirect('/');
+  });
+
+  // // replaced this hard-coded route with general static routing in app.js
+  // router.get('/stylesheets/style.css', function(req, res, next){
+  //   res.sendFile('/stylesheets/style.css', { root: __dirname + '/../public/' });
+  // });
+
+  return router;
+}
